000591e402d639101a8127e4c41ff2a0c746962d
yanzhaoliu <88018845+yanzhaoliu@users.noreply.github.com>
2023-04-03 08:32:32 +0800

    header

--- vanilla-tilt.js
+++ vanilla-tilt.js
@@ -417,7 +417,7 @@ var VanillaTilt = (function () {
        */
       extendSettings(settings) {
         let defaultSettings = {
-          reverse: false,
+          reverse: true,
           max: 5,
           startX: 0,
           startY: 0,
@@ -496,3 +496,21 @@ var VanillaTilt = (function () {
     return VanillaTilt;
     
     }());
+
+// When the user scrolls the page, execute myFunction
+window.onscroll = function() {myFunction()};
+
+// Get the header
+var header = document.getElementById("myHeader");
+
+// Get the offset position of the navbar
+var sticky = header.offsetTop;
+
+// Add the sticky class to the header when you reach its scroll position. Remove "sticky" when you leave the scroll position
+function myFunction() {
+  if (window.pageYOffset > sticky) {
+    header.classList.add("sticky");
+  } else {
+    header.classList.remove("sticky");
+  }
+}
